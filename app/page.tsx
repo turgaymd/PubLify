1c8726e014a94421a897665e5a8c1eaea44044d3
turgaymammadov <memmedovturqay871@gmail.com>
2025-06-01 12:45:42 +0400

    upload

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,62 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="container ">
+    <div className="auth flex flex-row">
+      <div className="left-side  w-full flex-1">
+        <Link href="/" className="logo">
+        <h2 className="font-bold text-3xl">PubLify</h2>
+        </Link>
+      </div>
+      <div className="right-side w-full flex-1">
+ <div className="switch flex justify-center mb-4 ">
+      <button className="log text-white font-bold">Login</button>
+      <button className="sign rounded-2xl font-bold border border-gray-200">Sign Up</button>
+    </div>
+      <form>
+        <div className="card flex flex-col  gap-3 rounded-2xl shadow-lg">
+          <div className="card-header px-2 py-5 text-white">
+            <h2 className="text-xl font-bold text-center">Sign Up</h2>
+          </div>
+          <div className="card-body flex flex-col px-2 py-2">
+            <div className="mb-3 flex flex-col">
+              <label htmlFor="" >Full Name</label> 
+              <input type="text" className="px-2 py-2  border border-gray-300 rounded-lg" placeholder="Arif Muradov"/>
+            </div>
+              <div className="mb-3 flex flex-col">
+              <label>Username</label> 
+              <input type="text" className="px-2 py-2  border border-gray-300 rounded-lg" placeholder="B.01.631.18.022"/>
+            </div>
+            <div className="mb-3 pb-3 flex flex-col">
+              <label htmlFor="email" >Email</label> 
+              <input type="email" className="px-2 py-2 w-full border border-gray-300 rounded-lg" placeholder="publify@gmail.com"/>
+            </div>
+            <div className="mb-3 flex flex-col">
+              <label htmlFor="password">Password</label> 
+              <input type="password" className="px-2 py-2  border border-gray-300 rounded-lg" placeholder="12345678"/>
+            </div>
+            <div className="terms inline-flex gap-2 items-center">
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <input type="checkbox" placeholder="s" className="bg-blue-500"/>
+            <span className="text-sm text-gray-500">  Agree to our Terms of use and Privacy Policy </span>
+            </div>
+            <div className="submit-btn text-center rounded-lg mb-5">
+            <button className="bg-blue-500 px-7 text-white py-3 font-bold  ">
+             Sign up
+            </button>
+            </div>
+  
+          <div>
+         
+          </div>
+          </div>
+        
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </form>
+      </div>
+    </div>
+   
     </div>
   );
 }
